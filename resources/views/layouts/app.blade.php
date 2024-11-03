1a5ec07f51f61d705aa4e4edd613714b5fcd1858
Ramuel Gonzalez <ramuelcl@gmail.com>
2024-11-03 18:56:31 +0100

    main, navigation user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,57 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+  <title>{{ config('guzanet.appEmpresa', 'Guzanet.') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.bunny.net">
+  <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+  <!-- Scripts -->
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  @livewireStyles
+</head>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+<body class="font-sans antialiased bg-lightBg text-lightText dark:bg-darkBg dark:text-darkText">
+  <div class="min-h-screen">
+    <!-- Page Heading -->
+    <header class="bg-lightBg text-lightText dark:bg-darkBg dark:text-darkText shadow">
+      @include('layouts.navigation')
+      @isset($header)
+      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        {{ $header }}
+      </div>
+      @endisset
+    </header>
+
+    <!-- Page Content -->
+    <main>
+      {{-- Contenido para Livewire o Blade --}}
+      @isset($slot)
+      {{ $slot }}
+      <!-- Para componentes Livewire -->
+      @else
+      @yield('content')
+      <!-- Para vistas Blade normales -->
+      @endisset
+    </main>
+
+    @isset($footer)
+    <footer class="bg-lightBg text-lightText dark:bg-darkBg dark:text-darkText">
+      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        {{ $footer }}
+      </div>
+    </footer>
+    @endisset
+
+  </div>
+  @livewireScripts
+
+</body>
+
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,33 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+  darkMode: 'class', // Usa la clase 'dark' para activar el modo oscuro
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        // sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+        'noto': ["'Noto Sans', sans-serif"],
+      },
+      colors: {
+        darkBg: '#1a202c',
+        darkText: '#ffffff',
+        lightBg: '#ffffff',
+        lightText: '#1a202c',
+        success: 'bg-green-500',
+        danger: 'bg-red-500',
+        warning: 'bg-yellow-500',
+        info: 'bg-blue-500'
+      },
     },
-
-    plugins: [forms],
+  },
+  plugins: [],
 };
