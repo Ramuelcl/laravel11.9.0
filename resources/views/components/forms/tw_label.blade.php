3f347d5e6b3d43ea9292abdb71c74478601ec89c
Ramuel Gonzalez <ramuelcl@gmail.com>
2024-11-07 00:31:26 +0100

    main - label para dark corregido

--- resources/views/components/forms/tw_label.blade.php
+++ resources/views/components/forms/tw_label.blade.php
@@ -2,7 +2,8 @@
 
 @props(['id' => '', 'name' => '', 'class' => ''])
 
-<label for="{{ $id ?? $name }}" {{ $attributes->merge(['class' => 'block text-sm font-medium leading-6 text-gray-900
+<label for="{{ $id ?? $name }}" {{ $attributes->merge(['class' => 'block text-sm font-medium leading-6 text-lightText
+  dark:text-darkText
   rounded-md' . $class]) }}
   >
   {{ $slot }}
